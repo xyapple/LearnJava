8972420fd7dd084842d8d0fda8b2e1a3f459ce2a
xyapple <ydeascentis@gmail.com>
2017-09-11 22:46:28 -0400

    Conditional Operator

---
+++ ConditionalOperator.java
@@ -0,0 +1,8 @@
+public class ConditionalOperator {
+    public static void main(String[] args){
+        int score=68;
+        String mark = (score >= 60) ? "及格":"不及格";
+        System.out.println("考试成绩如何："+mark);
+
+    }
+}
---
+++ OddNum.java
@@ -0,0 +1,9 @@
+public class OddNum {
+    public static void main(String[] args){
+        int one = 20;
+        if (one % 2 == 0){
+            System.out.println(one + " is odd");
+        }
+
+    }
+}
