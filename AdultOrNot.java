19c68b18b28416ea16bf74d91c1d313ab9dbca76
xyapple <ydeascentis@gmail.com>
2017-09-11 23:03:37 -0400

    Adult or not

---
+++ AdultOrNot.java
@@ -0,0 +1,12 @@
+public class AdultOrNot {
+    public static void main(String[] args) {
+        int age=25;
+
+        if (age >= 18){
+            System.out.println("if you are older than 18，then you are an adult.");
+        } else {
+            System.out.println("if you are not older than 18，then you are not an adult.");
+        }
+
+    }
+}
