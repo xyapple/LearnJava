21db5262ac44020dd8696c637d532d942687f870
xyapple <ydeascentis@gmail.com>
2017-09-11 23:09:43 -0400

    Update the if statement

--- AgeGroup.java
+++ AgeGroup.java
@@ -1,11 +1,11 @@
 public class AgeGroup {
     public static void main(String[] args) {
-        int age=25;
+        int age=31;
         if (age > 60){
             System.out.println("Elder");
-        } else if (age > 40){
+        } else if (60 >= age && age > 40){
             System.out.println("Adult");
-        } else if (age >18){
+        } else if (40 > age && age >18){
             System.out.println("Young Adult");
         } else{
             System.out.println("Youth");
