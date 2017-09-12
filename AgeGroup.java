dd8111430414982164402be37e201997e1c7ed22
xyapple <ydeascentis@gmail.com>
2017-09-11 23:03:58 -0400

    what is your age group

---
+++ AgeGroup.java
@@ -0,0 +1,15 @@
+public class AgeGroup {
+    public static void main(String[] args) {
+        int age=25;
+        if (age > 60){
+            System.out.println("Elder");
+        } else if (age > 40){
+            System.out.println("Adult");
+        } else if (age >18){
+            System.out.println("Young Adult");
+        } else{
+            System.out.println("Youth");
+        }
+
+    }
+}
