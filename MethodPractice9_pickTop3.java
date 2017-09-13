c8ced019de0476e7ccce7c4a04763df5a229b025
xyapple <ydeascentis@gmail.com>
2017-09-13 14:50:50 -0400

    Get the top three scores/numbers from the array from User Input, then save the user input into Array.

--- MethodPractice9_pickTop3.java
+++ MethodPractice9_pickTop3.java
@@ -1,16 +1,26 @@
 //pick the top three numbers
 import java.util.Arrays;
-//import java.util.Scanner;
+import java.util.Scanner;
 public class MethodPractice9_pickTop3 {
     public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Please enter an Array of numbers and separate them by comma: ");
+        String userInputNum = userInput.next();
+        String[] userInputArray = userInputNum.split(",");
+        int[] scores = new int[userInputArray.length];
+        for(int i = 0; i<userInputArray.length; i++){
+            scores[i] = Integer.parseInt(userInputArray[i]);
+            //System.out.print("User had enter: "+ scores[i]+" ");
+        }
+
         System.out.println("The top 3 scores are: ");
 
-        int[] scores = {89,-23,64,91,119,52,73};
+        //int[] scores = {89,-23,64,91,119,52,73};
 
         MethodPractice9_pickTop3 test = new MethodPractice9_pickTop3();
 
         test.getTopScores(scores);
-
 
     }
     //get the top 3 numbers from a Array; Print the top 3 from array then Break!
