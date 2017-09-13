dc875999bd978a53ac49b8e69423445e99e1eea4
xyapple <ydeascentis@gmail.com>
2017-09-12 23:03:55 -0400

    Loop through the Array

---
+++ LoopThroughArray.java
@@ -0,0 +1,10 @@
+public class LoopThroughArray {
+    public static void main(String[] args) {
+        String[] hobbys = {"sports", "game", "movie"};
+        System.out.println("Please loop though the hobbys: ");
+        for(int i=0; i<= hobbys.length; i++){
+            System.out.println(hobbys[i]);
+
+        }
+    }
+}
