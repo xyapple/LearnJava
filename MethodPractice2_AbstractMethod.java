3f428280efa6808d78e84aadf90864b02cf1828d
xyapple <ydeascentis@gmail.com>
2017-09-13 11:01:20 -0400

    Abstract Method in the Class

---
+++ MethodPractice2_AbstractMethod.java
@@ -0,0 +1,11 @@
+public class MethodPractice2_AbstractMethod {
+    public static void main(String[] args) {
+        MethodPractice2_AbstractMethod hello = new MethodPractice2_AbstractMethod();
+        hello.showMyLove();
+
+    }
+    public void showMyLove(){
+        System.out.println("Hello, Java~~ ");
+
+    }
+}
