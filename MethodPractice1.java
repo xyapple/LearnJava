3798cdbdea6e8cdf76898f70d1e6e94069ffb4f7
xyapple <ydeascentis@gmail.com>
2017-09-13 10:51:51 -0400

    First time to use Method in Java

---
+++ MethodPractice1.java
@@ -0,0 +1,10 @@
+public class MethodPractice1 {
+    public void print(){
+        System.out.println("Let's learn about method in Java!");
+    }
+
+    public static void main(String[] args) {
+        MethodPractice1 test = new MethodPractice1();
+        test.print();
+    }
+}
