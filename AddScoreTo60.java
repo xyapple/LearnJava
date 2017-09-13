b7c412243239257000e6bf15a22bdda08dc18844
xyapple <ydeascentis@gmail.com>
2017-09-12 20:58:24 -0400

    Update the program to take user input

--- AddScoreTo60.java
+++ AddScoreTo60.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 public class AddScoreTo60 {
     public static void main(String[] args){
-        // 变量保存成绩
-        int score = 53;
-        // 变量保存加分次数
+        Scanner input = new Scanner(System.in); //take user input
+        System.out.print("Please enter your score: ");
+
+        int score = input.nextInt(); //user has to enter previous score
         int count = 0;
-        //打印输出加分前成绩
         System.out.println("Previous score: " + score);
-        // 只要成绩小于60，就循环执行加分操作，并统计加分次数
+
         while(score < 60){
             count++;
             score++;
@@ -15,8 +17,6 @@ public class AddScoreTo60 {
 //            }
 
         }
-        //打印输出加分后成绩，以及加分次数
-        //System.out.println("Previous score: " + score);
         System.out.println("Current score: " + score);
         System.out.println("Total count: " + count);
     }
