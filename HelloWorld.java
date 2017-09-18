0f075c334ce8e7a862afa4ed2672788b1743821c
xyapple <ydeascentis@gmail.com>
2017-09-18 14:49:15 -0400

    Update Java OOP program.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,19 @@
 public class HelloWorld {
-   public static void main(String[] args){
-       System.out.println("I start to learn Java!");
-   }
+    public static void main(String[] args) {
+//       int x = 7;
+//       int y = x;
+//       x=2;
+//       System.out.println(x+","+y);
+        int a = 5;
+        int b = 7;
+        swap(a, b);
+        System.out.println(a + ", " + b);
+        System.out.println("I start to learn Java!");
+    }
+
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
 }
---
+++ Thing.java
@@ -0,0 +1,22 @@
+public class Thing{
+    private int a;
+    public Thing(int x){
+        a = x;
+    }
+    public int geta(){
+        return a;
+    }
+    public void combine(Thing y){
+        a = a + y.geta();
+    }
+
+
+    public static void main(String[] args) {
+        Thing f = new Thing(6);
+        Thing g = new Thing(8);
+        f.combine(g);
+        System.out.println(f.geta());
+        System.out.println(g.geta());
+    }
+
+}
